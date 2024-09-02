270d21c21188f5554e8b89534feb997a2eb72438
Brian Kim <Kibeom.kim@tufts.edu>
2024-09-02 21:59:38 +0900

    Create rectangle.c

---
+++ rectangle.c
@@ -0,0 +1,28 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    
+    int a;
+    scanf("%d", &a);
+    
+for(int i=0;i<a;i++){    
+    for(int j=0;j<a;j++){
+        if(i == 0 || i == a-1 || j==0 ||j == a-1){
+            printf("*");
+        }else{
+            printf(" ");
+        }
+    }
+    printf("\n");
+}
+    return 0;
+}
+
+Output:
+5
+*****
+*   *
+*   *
+*   *
+*****
